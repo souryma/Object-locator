c283f2079ef16d58b217e735d19ba2f3f050cd1d
Antoine <antoine.martin885@gmail.com>
2022-12-02 14:24:45 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+    import Distance from './lib/Distance.svelte';
+    import Geolocation from './lib/Geolocation.svelte';
+    import GeolocationWatch from './lib/GeolocationWatch.svelte';
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Geolocation positionNumber={1}/>
+    <Geolocation positionNumber={2}/>
+    <Distance lat1={45.6538353} lat2={45.6538355} lon1={0.1486953} lon2={0.1486953}/>  
+    <GeolocationWatch display={false}/>
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .card{
+    display: flex;
+    justify-content: space-around;
+  }
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/lib/Geolocation.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    export let positionNumber: number = 0
+    let latitude: number = 0
+    let longitude: number = 0
+
+    const getValues = (lat: number, lon: number) => {
+        latitude = lat
+        longitude = lon
+    }
+    
+    const getPosition = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            getValues(position.coords.latitude, position.coords.longitude);
+        });
+    }    
+</script>
+
+<div class="position">
+    {#if 'geolocation' in navigator}
+        <button on:click={getPosition}>
+            Get position {positionNumber}
+        </button>
+        <p>Your latitude : {latitude}</p>
+        <p>Your longitude : {longitude}</p>
+    {:else}
+        <p>This navigator is not supporting geolocalisation.</p>
+    {/if}
+</div>
+
+<style>
+    .position {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
