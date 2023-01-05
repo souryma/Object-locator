e114e2b0597f97e4769d0ef747d92b314c4e55fa
Antoine <antoine.martin885@gmail.com>
2023-01-05 15:13:16 +0100

    Removed VITE style + added new angle calculation method

--- src/App.svelte
+++ src/App.svelte
@@ -48,45 +48,55 @@
     // calculate the result
     distanceBetweenPositions = c * r;
 
-    distanceInKM = parseInt(String(distanceBetweenPositions))
-    distanceInM = parseInt(String((distanceBetweenPositions - distanceInKM) * 1000));
-    distanceInCM = parseInt(String((((distanceBetweenPositions - distanceInKM) * 1000) - distanceInM) * 100));
+    distanceInKM = parseInt(String(distanceBetweenPositions));
+    distanceInM = parseInt(
+      String((distanceBetweenPositions - distanceInKM) * 1000)
+    );
+    distanceInCM = parseInt(
+      String(
+        ((distanceBetweenPositions - distanceInKM) * 1000 - distanceInM) * 100
+      )
+    );
   };
 
   let angle: number = 100;
 
   const orientation = () => {
-
     let deltaLongitudes = watcher_position[1] - object_position[1];
     let pointA = Math.cos(object_position[0]) * Math.sin(deltaLongitudes);
-    let pointB = Math.cos(watcher_position[0]) * Math.sin(object_position[0]) - Math.sin(watcher_position[0]) * Math.cos(object_position[0]) * Math.cos(deltaLongitudes);
+    let pointB =
+      Math.cos(watcher_position[0]) * Math.sin(object_position[0]) -
+      Math.sin(watcher_position[0]) *
+        Math.cos(object_position[0]) *
+        Math.cos(deltaLongitudes);
 
     angle = Math.atan2(pointA, pointB);
-  }
+  };
+
+  const angleFromCoordinate = () => {
+    // Returns angle in degrees based on coordinates
+    angle = (Math.atan2(object_position[1] - watcher_position[1], object_position[0] - watcher_position[0]) * 180) / Math.PI;
+  };
 
   let ms = 1000;
   let clear;
   $: {
     clearInterval(clear);
     clear = setInterval(distance, ms);
-    clear = setInterval(orientation, ms);
+    clear = setInterval(angleFromCoordinate, ms);
   }
 </script>
 
 <main>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src="/vite.svg" class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
-  </div>
-  <h1>Vite + svelte</h1>
-
-  <!-- DEBUG : Distance update speed -->
-  Distance update speed :
-  <input type="number" bind:value={ms} />
+  </div> -->
+  <h1>Locate an object</h1>
 
   {#if "geolocation" in navigator}
     <div class="card">
@@ -99,7 +109,7 @@
       </div>
 
       <div class="position">
-        <button on:click={getObjectPosition}> Set your object position </button>
+        <button on:click={getObjectPosition}>Set your object position</button>
         <div class="coords">
           <p>Object latitude : {object_position[0]}</p>
           <p>Object longitude : {object_position[1]}</p>
@@ -107,7 +117,10 @@
       </div>
 
       <div class="coords">
-        <p>Distance to your object : {#if distanceInKM != 0}{distanceInKM} Km / {/if}{#if distanceInM != 0}{distanceInM} m / {/if}{distanceInCM} cm</p>
+        <p>
+          Distance to your object : {#if distanceInKM != 0}{distanceInKM} Km /
+          {/if}{#if distanceInM != 0}{distanceInM} m / {/if}{distanceInCM} cm
+        </p>
       </div>
       <div class="coords">
         <p>Angle to your object : {angle}°</p>
@@ -128,16 +141,6 @@
   {:else}
     <p>The navigator doesn't have Geolocation</p>
   {/if}
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
@@ -166,8 +169,5 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
