762604e1dc4576af5f39b21871e9c8c85959015f
Antoine <antoine.martin885@gmail.com>
2023-01-08 13:12:50 +0100

    Tests with device orientation and compass heading

--- src/App.svelte
+++ src/App.svelte
@@ -335,13 +335,13 @@
               src="/Dark_Green_Arrow_Up.png"
               class="logo"
               alt="Direction to the object"
-              style="transform: rotate({compassHeading - angleToObject}deg)"
+              style="transform: rotate({(compassHeading - deviceOrientation) + angleToObject}deg)"
             />
 
           </a>
         </div>
 
-        <p>Image rotation : {compassHeading - angleToObject} deg</p>
+        <p>Image rotation : {(compassHeading - deviceOrientation) + angleToObject} deg</p>
 
         <div class="coordinates">
           <div class="coords">
